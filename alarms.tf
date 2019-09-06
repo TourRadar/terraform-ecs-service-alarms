fc13cb079e1e93fc2122c0000c17e3b5ba3d326e
Chris Bednarczyk <chris@tourradar.com>
2019-09-06 11:56:16 +0200

    Initial commit.

--- alarms.tf
+++ alarms.tf
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_high" {
   namespace           = "AWS/ECS"
   period              = var.cpu_utilization_high_evaluation_period
   statistic           = "Average"
-  alarm_description   = "Priority: P${each.key}  CPU usage for ecs service is too high. Please check service"
+  alarm_description   = "Priority: ${each.key}  CPU usage for ecs service is too high. Please check service"
   treat_missing_data  = var.cpu_utilization_high_treat_missing_data
   alarm_actions       = var.actions
   ok_actions          = var.ok_actions
@@ -26,7 +26,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_high" {
 
 resource "aws_cloudwatch_metric_alarm" "memory_utilization_high" {
   for_each            = var.memory_utilization_high_checks
-  alarm_name          = "${var.alarm_prefix}: ECS service cpu utilization is too high"
+  alarm_name          = "${var.alarm_prefix}: ECS service memory utilization is too high"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = var.memory_utilization_high_evaluation_periods
   threshold           = each.value
@@ -34,7 +34,7 @@ resource "aws_cloudwatch_metric_alarm" "memory_utilization_high" {
   namespace           = "AWS/ECS"
   period              = var.memory_utilization_high_evaluation_period
   statistic           = "Average"
-  alarm_description   = "Priority: P${each.key}  CPU usage for ecs service is too high. Please check service"
+  alarm_description   = "Priority: ${each.key}  memory usage for ecs service is too high. Please check service"
   treat_missing_data  = var.memory_utilization_high_treat_missing_data
   alarm_actions       = var.actions
   ok_actions          = var.ok_actions
